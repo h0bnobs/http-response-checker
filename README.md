5c079d911a5265a66ffca1613993fa68ad241917
h0bnobs <mb8ld0ck@gmail.com>
2024-08-15 18:21:47 +0100

    Removed -nO and replaced it with -q.
    Removed the second call to get_cookies for each url and cleaned up main more.
    
    Added requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# http-response-checker
-Checks http responses for secure headers and cookie attributes.
+# HTTP response checker that looks for secure headers and cookie attributes
+Makes a request to a url and checks the response against a [list of security headers](https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html).<br>It then gets the cookies that the page uses. 
+## Usage
+```
+git clone https://github.com/h0bnobs/security-response-checker
+cd security-response-checker
+pip install -r requirements.txt
+python security-response-checker
+```
+## Examples
+Print the headers and cookie data of one url:
+```
+python security-response-checker -u https://google.com
+```
+Print the headers and cookie data of multiple urls in a txt file and output them in a readable format a file:
+```
+python security-response-checker -i target_urls.txt -o output_file.txt
+```
+Print nothing to the terminal but send the output to a file:
+```
+python security-repsponse-checker -i target_urls.txt -q -o output_file.txt
+```
+
+## Information
+There are 3 types of input files accepted: [dirb's](https://www.kali.org/tools/dirb/) output file, [ffuf's](https://github.com/ffuf/ffuf) output file and a simple txt file, with each row containing a URL. 
--- security-header-checker.py
+++ security-header-checker.py
@@ -1,16 +1,12 @@
 import argparse
-from wsgiref import headers
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import json
 import requests
 from termcolor import colored
-from urllib.parse import urlparse
 import re
 import json
-import os
 import ast
 import sys
 
@@ -56,29 +52,32 @@ security_headers = {
 }
 
 
-# requirements : import requests, from termcolor import colored, from selenium import webdriver, from webdriver_manager.chrome import ChromeDriverManager
-
 def banner():
-    text = r"""
-       .__            
-  _____|  |__   ____  
- /  ___/  |  \_/ ___\ 
- \___ \|   Y  \  \___ 
-/____  >___|  /\___  >
-     \/     \/     \/ 
-@h0bnobs 
-usage: security-header-checker -u <url> -i <input_file> -o <output_file> -nO <output_file>
-"""
-    print(text)
+    banner = """
+     _          
+    | |         
+ ___| |__   ___ 
+/ __| '_ \ / __|
+\__ \ | | | (__ 
+|___/_| |_|\___|
+
+https://github.com/h0bnobs/http-response-checker
+
+usage: security-header-checker.py -i <input_file> -u <target_url> -q -o <output_file>
+    """
+    print(colored(banner, 'yellow'))
 
 
 def parse_args():
+    """
+    Parses command line arguments.
+    :return: The arguments parsed from the command line.
+    """
     parser = argparse.ArgumentParser(description='Security header checker')
-    parser.add_argument('-u', '--url', dest="target", help='The target URL to be scanned')
+    parser.add_argument('-u', '--url', dest="target", help='The single target URL to be scanned')
     parser.add_argument('-i', '--input_file', dest="input_file", help='Input file')
     parser.add_argument('-o', '--output_file', dest="output_file", help='Sends output to a file')
-    parser.add_argument('-nO', '--no-print-output', dest="no_print_output", help='Prints far less to terminal and '
-                                                                                 'sends to output file')
+    parser.add_argument('-q', '--quiet', dest="quiet", action='store_true', help='Prints far less to terminal')
     return parser.parse_args()
 
 
@@ -159,66 +158,43 @@ def output_type(filename):
             return 'ffuf'
 
 
-def print_cookies(target_url):
+def print_cookies(cookies, target_url):
     """
     Prints the cookies of the target page. Uses selenium to find the cookies, meaning it'll flash a chrome window up.
     :param target_url: The url of the page.
     """
-    # options for headless mode so it doesnt flash on screen - gpt special
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run in headless mode
-    chrome_options.add_argument("--no-sandbox")  # Required for some environments
-    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    try:
-        driver.get(target_url)
-        cookies = driver.get_cookies()
-        final_cookies = []
-        for dictionary in cookies:
-            temp_dictionary = {
-                "name": dictionary["name"],
-                "value": dictionary["value"],
-                "httpOnly": dictionary['httpOnly'],
-                "secure": dictionary['secure']
-            }
-            final_cookies.append(temp_dictionary)
-    finally:
-        driver.quit()
 
     # nice printing
-    if len(final_cookies) > 0:
-        if len(final_cookies) == 1:
+    if len(cookies) > 0:
+        if len(cookies) == 1:
             print("\n " + COLOURS["plus"] + " The following cookie was found on " + target_url + ":" + COLOURS["end"])
-            print(json.dumps(final_cookies, indent=4))
+            print(json.dumps(cookies, indent=4))
         else:
             print("\n " + COLOURS["plus"] + " The following " + str(
-                len(final_cookies)) + " cookies were found on " + target_url + ":" + COLOURS["end"])
-            print(json.dumps(final_cookies, indent=4))
+                len(cookies)) + " cookies were found on " + target_url + ":" + COLOURS["end"])
+            print(json.dumps(cookies, indent=4))
     else:
         print("\n " + COLOURS["cross"] + " No cookies found on " + target_url + COLOURS["end"])
 
 
-def print_headers(target_url):
+def print_headers(headers, target_url):
     """
     Prints the security headers that match both owasp's HTTP security headers list and those that are used by the page.
     :param target_url: The page that was scanned.
     """
     count = 0
-    response = requests.get(target_url)
-    headers = response.headers
-
     # pretty printing logic
     pretty_print = {}
-    print("\n" + COLOURS["plus"] + " Here are the headers from " + target_url + " for inspection:" + COLOURS["end"] + "\n")
+    print("\n" + COLOURS["plus"] + " Here are the headers from " + target_url + " for inspection:" + COLOURS[
+        "end"] + "\n")
     for header in headers:
         # if there are security headers present, we need to see them so that we can
         if header in security_headers.keys():
             count += 1
-            print(str(count) + ") The following header: " + colored("" + header + ": " + headers[header],
-                                                          "red") + " is present on this page")
+            print(str(count) + ") " + colored("" + header + ": " + headers[header], "red"))
             pretty_print.update({str(count) + ") " + header: security_headers[header]})
-    print("\n" + COLOURS["plus"] + " Here are the recommendations for the security headers that were found on " + target_url + ":" + COLOURS["end"] + "\n")
+    print("\n" + COLOURS["plus"] + " Here are the recommendations for the security headers that were found on " + ":" +
+          COLOURS["end"] + "\n")
     print_pretty(pretty_print)
 
 
@@ -233,7 +209,7 @@ def get_headers(target_url):
     h = {}
     for header in headers:
         if header in security_headers.keys():
-            h.update({header: security_headers[header]})
+            h.update({header: headers[header]})
     return h
 
 
@@ -292,43 +268,68 @@ if __name__ == '__main__':
     banner()
     args = parse_args()
 
-    if args.no_print_output and args.output_file:
-        print("-o and -nO cannot be used at the same time!")
+    if args.quiet and not args.output_file:
+        print(COLOURS["warn"] + " Cannot use -q without -o!")
         sys.exit()
 
-    def create_or_empty_file(filepath):
-        if filepath:
-            with open(filepath, 'w') as file:
+
+    def create_or_empty_file(filename):
+        """
+        Creates a file if it doesn't exist, or it wipes it if it does exist.
+        :param filename: The name of the file to be created/wiped.
+        :return:
+        """
+        if filename:
+            with open(filename, 'w') as file:
                 file.write('')
 
-    create_or_empty_file(args.output_file)
-    create_or_empty_file(args.no_print_output)
+
+    if args.output_file:
+        create_or_empty_file(args.output_file)
+
 
     def process_url(url):
-        formatted_headers = json.dumps(get_headers(url), indent=4)
-        formatted_cookies = json.dumps(get_cookies(url), indent=4)
+        """
+        Gets the headers and cookies for the inputted url. If -q and -o are present then it prints nothing. Otherwise,
+        it prints, then if -o is present it outputs to a file.
+        :param url: The target url to be checked.
+        """
+        unformatted_headers = get_headers(url)
+        unformatted_cookies = get_cookies(url)
+
+        formatted_headers = json.dumps(unformatted_headers, indent=4)
+        formatted_cookies = json.dumps(unformatted_cookies, indent=4)
         output_content = (
             f"\nHeaders for {url}:\n{formatted_headers}\n"
             f"\nCookies for {url}:\n{formatted_cookies}\n"
         )
-        if args.no_print_output:
-            with open(args.no_print_output, 'a') as file:
+
+        if args.quiet and args.output_file:
+            with open(args.output_file, 'a') as file:
                 file.write(output_content)
+                return
         else:
-            print_headers(url)
-            print_cookies(url)
+            print_headers(unformatted_headers, url)
+            print_cookies(unformatted_cookies, url)
             if args.output_file:
                 with open(args.output_file, 'a') as file:
                     file.write(output_content)
 
 
+    # if -u is used
     if args.target:
         process_url(args.target)
 
+    # if -i is used
     if args.input_file:
-        tool_name = output_type(args.input_file)
-        with open(args.input_file, 'r') as file:
-            data = file.read()
+        try:
+            tool_name = output_type(args.input_file)
+            with open(args.input_file, 'r') as file:
+                data = file.read()
+        except FileNotFoundError:
+            print("\n" + COLOURS[
+                "warn"] + " FileNotFoundError: [Errno 2] No such file or directory: '" + args.input_file + "'")
+            sys.exit()
 
         urls = []
         if tool_name == 'ffuf':
@@ -337,6 +338,22 @@ if __name__ == '__main__':
             urls = dirb_parser(args.input_file)
         elif args.input_file.endswith('.txt'):
             urls = [line.strip() for line in data.splitlines()]
+        elif tool_name is None and not args.input_file.endswith('.txt'):
+            print("\n" + COLOURS["warn"] + " Input file not recognised/supported/correct!" + COLOURS["end"])
+            message = (
+                f"\n{colored('Currently only supporting ', 'red')}"
+                f"{colored('dirb', 'green')}"
+                f"{colored(' and ', 'red')}"
+                f"{colored('ffuf', 'green')}"
+                f"{colored(' outputs and ', 'red')}"
+                f"{colored('.txt', 'green')}"
+                f"{colored(' files in the following format:', 'red')}\n\n"
+                "https://example.com/\n"
+                "https://example.com/page"
+                f"\n\n{colored('I would recommend not including pages that responded with 403 or 500', 'red')}"
+            )
+            print(message)
+            sys.exit()
 
         count = 0
         total_urls = len(urls)
@@ -355,13 +372,11 @@ if __name__ == '__main__':
 
             if total_urls >= 8:
                 if count > quarter_mark and not printed_quarter:
-                    print(colored('\n\n### Quarter of the way through ###\n', 'green'))
+                    print(colored('\n\n### Quarter of the way through ###\n', 'yellow'))
                     printed_quarter = True
                 if count > half_mark and not printed_half:
-                    print(colored('\n\n### Halfway through ###\n', 'green'))
+                    print(colored('\n\n### Halfway through ###\n', 'yellow'))
                     printed_half = True
                 if count > three_quarters_mark and not printed_three_quarters:
-                    print(colored('\n\n### Three quarters of the way through ###\n', 'green'))
+                    print(colored('\n\n### Three quarters of the way through ###\n', 'yellow'))
                     printed_three_quarters = True
-
-    print(colored('\nAll done! Finishing up...', 'cyan'))
